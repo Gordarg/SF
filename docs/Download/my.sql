fceac9ca2d5a474f9121e825140ce38b95f10cdb
tayyebi <tayyebi_mohammadreza@hotmail.com>
2020-05-21 12:35:39 +0430

    Moved database

--- docs/Download/my.sql
+++ docs/Download/my.sql
@@ -1,77 +1,387 @@
-CREATE TABLE `Posts` (
-    `Id` INT NOT NULL AUTO_INCREMENT ,
-    `Title` VARCHAR(3000) NOT NULL ,
-    `Submit` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ,
-    `Abstract` MEDIUMTEXT NOT NULL ,
-    PRIMARY KEY (`Id`)) ENGINE = InnoDB;
-ALTER TABLE `Posts` CHANGE `Abstract` `Abstract` MEDIUMTEXT CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL;
-ALTER TABLE `Posts` ADD `Body` LONGTEXT NOT NULL AFTER `Abstract`;
-ALTER TABLE `Posts` ADD `Type` VARCHAR(20) NULL AFTER `Body`;
-ALTER TABLE `Posts` ADD `FileName` VARCHAR(3000) NULL AFTER `Type`;
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: May 21, 2020 at 07:50 AM
+-- Server version: 5.7.29-0ubuntu0.18.04.1
+-- PHP Version: 7.2.24-0ubuntu0.18.04.4
 
-CREATE TABLE `Files` (
- `Id` int(11) NOT NULL AUTO_INCREMENT,
- `FileName` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
- `Submit` datetime NOT NULL,
- `Status` enum('1','0') COLLATE utf8_unicode_ci NOT NULL DEFAULT '1',
- PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
-CREATE TABLE `Users` ( `Email` VARCHAR(255) NOT NULL , `Password` VARCHAR(3000) NOT NULL ) ENGINE = InnoDB;
-ALTER TABLE `Users` ADD `Id` INT NOT NULL AUTO_INCREMENT FIRST, ADD PRIMARY KEY (`Id`);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-CREATE TABLE `Visits` (
-    `Id` BIGINT NOT NULL AUTO_INCREMENT ,
-    `Submit` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ,
-    `CONTEXT_DOCUMENT_ROOT` VARCHAR(500) NULL ,
-    `CONTEXT_PREFIX` VARCHAR(500) NULL ,
-    `DOCUMENT_ROOT` VARCHAR(500) NULL ,
-    `GATEWAY_INTERFACE` VARCHAR(500) NULL,
-    `HTTP_ACCEPT` VARCHAR(500) NULL,
-    `HTTP_ACCEPT_ENCODING` VARCHAR(500) NULL,
-    `HTTP_ACCEPT_LANGUAGE` VARCHAR(500) NULL,
-    `HTTP_CACHE_CONTROL` VARCHAR(500) NULL,
-    `HTTP_CONNECTION` VARCHAR(500) NULL,
-    `HTTP_COOKIE` VARCHAR(500) NULL,
-    `HTTP_HOST` VARCHAR(500) NULL,
-    `HTTP_REFERER` VARCHAR(500) NULL,
-    `HTTP_SEC_FETCH_DEST` VARCHAR(500) NULL,
-    `HTTP_SEC_FETCH_MODE` VARCHAR(500) NULL,
-    `HTTP_SEC_FETCH_SITE` VARCHAR(500) NULL,
-    `HTTP_SEC_FETCH_USER` VARCHAR(500) NULL,
-    `HTTP_UPGRADE_INSECURE_REQUESTS` VARCHAR(500) NULL,
-    `HTTP_USER_AGENT` VARCHAR(500) NULL,
-    `PATH` VARCHAR(500) NULL,
-    `PATH_INFO` VARCHAR(500) NULL,
-    `PATH_TRANSLATED` VARCHAR(500) NULL,
-    `PHP_SELF` VARCHAR(500) NULL,
-    `QUERY_STRING` VARCHAR(500) NULL,
-    `REDIRECT_STATUS` VARCHAR(500) NULL,
-    `REDIRECT_URL` VARCHAR(500) NULL,
-    `REMOTE_ADDR` VARCHAR(500) NULL,
-    `REMOTE_PORT` VARCHAR(500) NULL,
-    `REQUEST_METHOD` VARCHAR(500) NULL,
-    `REQUEST_SCHEME` VARCHAR(500) NULL,
-    `REQUEST_TIME` VARCHAR(500) NULL,
-    `REQUEST_TIME_FLOAT` VARCHAR(500) NULL,
-    `REQUEST_URI` VARCHAR(500) NULL,
-    `SCRIPT_FILENAME` VARCHAR(500) NULL,
-    `SCRIPT_NAME` VARCHAR(500) NULL,
-    `SERVER_ADDR` VARCHAR(500) NULL,
-    `SERVER_ADMIN` VARCHAR(500) NULL,
-    `SERVER_NAME` VARCHAR(500) NULL,
-    `SERVER_PORT` VARCHAR(500) NULL,
-    `SERVER_PROTOCOL` VARCHAR(500) NULL,
-    `SERVER_SIGNATURE` VARCHAR(500) NULL,
-    `SERVER_SOFTWARE` VARCHAR(500) NULL,
-    PRIMARY KEY (`Id`)) ENGINE = InnoDB;
+--
+-- Database: `SnowKMS`
+--
 
+-- --------------------------------------------------------
 
+--
+-- Table structure for table `contacts`
+--
 
-CREATE TABLE `Comments` ( `Id` INT NOT NULL AUTO_INCREMENT , `Submit` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP , `Body` VARCHAR(3000) NOT NULL , `EMail` VARCHAR(320) NULL , `Name` varchar(200) NULL , `PostId` INT NOT NULL , PRIMARY KEY (`Id`)) ENGINE = InnoDB;
-ALTER TABLE `Comments` ADD CONSTRAINT `PostMustExist` FOREIGN KEY (`PostId`) REFERENCES `Posts`(`Id`) ON DELETE CASCADE ON UPDATE CASCADE;
-ALTER TABLE `Comments` ADD `IsFromAdmin` BIT(1) NOT NULL DEFAULT b'0' AFTER `Name`,
-ADD `IsPublic` BIT(1) NOT NULL DEFAULT b'1' AFTER `IsFromAdmin`,
-ADD `IsVisible` BIT(1) NOT NULL DEFAULT b'0' AFTER `IsPublic`,
-ADD `IsDeleted` BIT(1) NOT NULL DEFAULT b'0' AFTER `IsVisible`;
+CREATE TABLE `contacts` (
+  `Id` bigint(20) NOT NULL,
+  `UserId` int(11) NOT NULL,
+  `Type` varchar(300) NOT NULL,
+  `Value` varchar(300) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `departments`
+--
+
+CREATE TABLE `departments` (
+  `Id` int(11) NOT NULL,
+  `Name` varchar(3000) NOT NULL,
+  `IsPrivate` bit(1) NOT NULL DEFAULT b'0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `emails`
+--
+
+CREATE TABLE `emails` (
+  `Id` bigint(20) NOT NULL,
+  `IsOutbound` bit(1) NOT NULL,
+  `Title` varchar(3000) NOT NULL,
+  `ReceiveDate` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `RAW` longtext NOT NULL,
+  `MessageId` varchar(500) NOT NULL,
+  `ReplyId` varchar(500) NOT NULL,
+  `Sender` varchar(300) NOT NULL,
+  `Date` varchar(50) NOT NULL,
+  `Message` longtext NOT NULL,
+  `MessageNormal` longtext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `human-behaviour`
+--
+
+CREATE TABLE `human-behaviour` (
+  `Id` bigint(20) NOT NULL,
+  `Year` int(11) NOT NULL,
+  `Month` tinyint(4) NOT NULL,
+  `Day` tinyint(4) NOT NULL,
+  `From` time NOT NULL,
+  `To` time NOT NULL,
+  `Quality` tinyint(4) NOT NULL,
+  `Task` varchar(300) NOT NULL,
+  `Brief` varchar(4000) NOT NULL,
+  `UserId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `logs`
+--
+
+CREATE TABLE `logs` (
+  `Id` int(11) NOT NULL,
+  `Event` varchar(10) DEFAULT 'LOGIN',
+  `Key` varchar(100) DEFAULT 'USERNAME',
+  `Value` varchar(3000) DEFAULT 'TOKEN',
+  `Submit` datetime DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posts`
+--
+
+CREATE TABLE `posts` (
+  `MasterId` char(36) NOT NULL,
+  `Id` int(11) NOT NULL,
+  `Title` varchar(400) DEFAULT NULL,
+  `Submit` datetime NOT NULL,
+  `Type` char(5) NOT NULL DEFAULT 'POST' COMMENT 'POST, FILE, ARTL, COMT, SURV, QUST, ANSR,CHAT,TRNL,QUOT',
+  `Level` char(2) DEFAULT 'DC' COMMENT 'Data Content by default. Other SEO and publish levels must be declared with integrers.',
+  `BinContent` longblob,
+  `Body` longtext,
+  `UserId` int(11) DEFAULT NULL,
+  `Status` char(7) DEFAULT 'DRAFT' COMMENT 'Post lifecycle',
+  `Language` varchar(5) DEFAULT 'fa-IR',
+  `RefrenceId` char(36) DEFAULT NULL,
+  `Index` smallint(6) DEFAULT NULL,
+  `IsDeleted` bit(1) NOT NULL DEFAULT b'0',
+  `IsContentDeleted` bit(1) NOT NULL DEFAULT b'0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Stand-in structure for view `post_contributers`
+-- (See below for the actual view)
+--
+CREATE TABLE `post_contributers` (
+`MasterID` char(36)
+,`ID` int(11)
+,`UserID` int(11)
+,`Username` varchar(45)
+,`Submit` datetime
+,`Language` varchar(5)
+);
+
+-- --------------------------------------------------------
+
+--
+-- Stand-in structure for view `post_details`
+-- (See below for the actual view)
+--
+CREATE TABLE `post_details` (
+`MasterID` char(36)
+,`Title` varchar(400)
+,`ID` int(11)
+,`Submit` datetime
+,`UserID` int(11)
+,`Username` varchar(45)
+,`Body` longtext
+,`Type` char(5)
+,`Level` char(2)
+,`RefrenceID` char(36)
+,`Index` smallint(6)
+,`Status` char(7)
+,`Language` varchar(5)
+,`BinContent` longblob
+);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `staffs`
+--
+
+CREATE TABLE `staffs` (
+  `UserId` int(11) NOT NULL,
+  `DepartmentId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tickets`
+--
+
+CREATE TABLE `tickets` (
+  `Id` bigint(20) NOT NULL,
+  `ReplyId` bigint(20) DEFAULT NULL,
+  `Title` varchar(3000) DEFAULT NULL,
+  `EmailId` bigint(20) DEFAULT NULL,
+  `IsDeleted` bit(1) NOT NULL COMMENT 'Logical Delete',
+  `IsClosed` bit(1) NOT NULL,
+  `Priority` tinyint(4) NOT NULL DEFAULT '3' COMMENT '3 Means high',
+  `SenderEmail` varchar(300) DEFAULT NULL,
+  `UserId` int(11) DEFAULT NULL COMMENT 'Audience user',
+  `Message` longtext,
+  `File` longblob,
+  `DepartmentId` int(11) DEFAULT NULL,
+  `SubmitDate` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `AdminId` int(11) DEFAULT NULL,
+  `IsUserReceiver` bit(1) NOT NULL DEFAULT b'0' COMMENT 'If admin opens a ticket with user',
+  `ValidationCode` varchar(3000) DEFAULT NULL,
+  `IsValid` bit(1) NOT NULL DEFAULT b'0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `Id` int(11) NOT NULL,
+  `Username` varchar(45) DEFAULT NULL,
+  `HashPassword` tinytext,
+  `IsActive` bit(1) DEFAULT b'1',
+  `Role` tinyint(5) DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure for view `post_contributers`
+--
+DROP TABLE IF EXISTS `post_contributers`;
+
+CREATE VIEW `post_contributers`  AS  select `P`.`MasterId` AS `MasterID`,`P`.`Id` AS `ID`,`P`.`UserId` AS `UserID`,`U`.`Username` AS `Username`,`P`.`Submit` AS `Submit`,`P`.`Language` AS `Language` from (`posts` `P` join `users` `U` on((`P`.`UserId` = `U`.`Id`))) ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure for view `post_details`
+--
+DROP TABLE IF EXISTS `post_details`;
+
+CREATE VIEW `post_details`  AS  select `P`.`MasterId` AS `MasterID`,`P`.`Title` AS `Title`,`P`.`Id` AS `ID`,`P`.`Submit` AS `Submit`,`P`.`UserId` AS `UserID`,`U`.`Username` AS `Username`,`P`.`Body` AS `Body`,`P`.`Type` AS `Type`,`P`.`Level` AS `Level`,`P`.`RefrenceId` AS `RefrenceID`,`P`.`Index` AS `Index`,`P`.`Status` AS `Status`,`P`.`Language` AS `Language`,(case when ((select `P2`.`Submit` from `posts` `P2` where ((`P2`.`IsContentDeleted` = 1) and (`P`.`MasterId` = `P2`.`MasterId`)) order by `P2`.`Submit` desc limit 1) > (select `P1`.`Submit` from `posts` `P1` where ((`P1`.`BinContent` is not null) and (`P`.`MasterId` = `P1`.`MasterId`)) order by `P1`.`Submit` desc limit 1)) then NULL else (select `P1`.`BinContent` from `posts` `P1` where ((`P1`.`BinContent` is not null) and (`P`.`IsContentDeleted` = 0) and (`P`.`MasterId` = `P1`.`MasterId`)) order by `P1`.`Submit` desc limit 1) end) AS `BinContent` from (`posts` `P` join `users` `U` on((`P`.`UserId` = `U`.`Id`))) where (`P`.`Id` in (select max(`posts`.`Id`) from `posts` group by `posts`.`MasterId`,`posts`.`Language`) and (`P`.`IsDeleted` = '0')) ;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `contacts`
+--
+ALTER TABLE `contacts`
+  ADD PRIMARY KEY (`Id`),
+  ADD KEY `User_Contact` (`UserId`);
+
+--
+-- Indexes for table `departments`
+--
+ALTER TABLE `departments`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- Indexes for table `emails`
+--
+ALTER TABLE `emails`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- Indexes for table `human-behaviour`
+--
+ALTER TABLE `human-behaviour`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- Indexes for table `logs`
+--
+ALTER TABLE `logs`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- Indexes for table `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`Id`,`MasterId`),
+  ADD KEY `fk_posts_user_idx` (`UserId`);
+
+--
+-- Indexes for table `staffs`
+--
+ALTER TABLE `staffs`
+  ADD KEY `Staff_User` (`UserId`);
+
+--
+-- Indexes for table `tickets`
+--
+ALTER TABLE `tickets`
+  ADD PRIMARY KEY (`Id`),
+  ADD KEY `Email_Id` (`EmailId`),
+  ADD KEY `Reply_Id` (`ReplyId`),
+  ADD KEY `Admin_Id` (`AdminId`),
+  ADD KEY `User_Id` (`UserId`),
+  ADD KEY `Department_Id` (`DepartmentId`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`Id`),
+  ADD UNIQUE KEY `Username` (`Username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `contacts`
+--
+ALTER TABLE `contacts`
+  MODIFY `Id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `departments`
+--
+ALTER TABLE `departments`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `emails`
+--
+ALTER TABLE `emails`
+  MODIFY `Id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `human-behaviour`
+--
+ALTER TABLE `human-behaviour`
+  MODIFY `Id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `logs`
+--
+ALTER TABLE `logs`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tickets`
+--
+ALTER TABLE `tickets`
+  MODIFY `Id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `contacts`
+--
+ALTER TABLE `contacts`
+  ADD CONSTRAINT `User_Contact` FOREIGN KEY (`UserId`) REFERENCES `users` (`Id`);
+
+--
+-- Constraints for table `posts`
+--
+ALTER TABLE `posts`
+  ADD CONSTRAINT `fk_posts_user` FOREIGN KEY (`UserId`) REFERENCES `users` (`Id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `staffs`
+--
+ALTER TABLE `staffs`
+  ADD CONSTRAINT `Staff_User` FOREIGN KEY (`UserId`) REFERENCES `users` (`Id`);
+
+--
+-- Constraints for table `tickets`
+--
+ALTER TABLE `tickets`
+  ADD CONSTRAINT `Admin_Id` FOREIGN KEY (`AdminId`) REFERENCES `users` (`Id`),
+  ADD CONSTRAINT `Department_Id` FOREIGN KEY (`DepartmentId`) REFERENCES `departments` (`Id`),
+  ADD CONSTRAINT `Email_Id` FOREIGN KEY (`EmailId`) REFERENCES `emails` (`Id`),
+  ADD CONSTRAINT `Reply_Id` FOREIGN KEY (`ReplyId`) REFERENCES `tickets` (`Id`),
+  ADD CONSTRAINT `User_Id` FOREIGN KEY (`UserId`) REFERENCES `users` (`Id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
